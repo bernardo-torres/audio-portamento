dc9e4021ffbcf465b5d801c00a3f4769e5b114ae
torresbf <beftorres@hotmail.com>
2022-01-19 15:12:46 +0100

    Deteting old utils versions and audio files in main folder

--- audio_transport/spec_utils.py
+++
@@ -1,8 +0,0 @@
-import librosa
-import numpy as np
-
-def stft(wave):
-    s = librosa.stft(wave, n_fft=1024, win_length=512, hop_length= int(512 / 2))
-    S = np.abs(s)
-    S_db = librosa.amplitude_to_db(S, ref=np.max)
-    return s, S_db 
